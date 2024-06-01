595cb996712e92c64e7465d7d688c49a7000e7de
casss-dev <bcassidy50@gmail.com>
2024-06-01 13:42:48 -0500

    Fix finding LSP root_dir in swift projects

--- lua/user/lspsettings/sourcekit.lua
+++ lua/user/lspsettings/sourcekit.lua
@@ -2,4 +2,10 @@
 return {
   cmd = { '/usr/bin/sourcekit-lsp' },
   -- cmd = { '/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/sourcekit-lsp' },
+  root_dir = function(filename, bufnr)
+    -- Below fixes lsp root_dir when modules are nested within xcode projects
+    -- This default to using the correct root_dir based vim's working directory
+    local util = require 'lspconfig.util'
+    return util.root_pattern('Package.swift', '*.xcodeproj', '*.xcworkspace')(filename)
+  end,
 }
