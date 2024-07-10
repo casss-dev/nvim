90ded044d2396d7bb0c14417d1c47a5790cad488
casss-dev <bcassidy50@gmail.com>
2024-07-10 05:18:00 -0500

    Fix sourcekit

--- lua/user/lspsettings/sourcekit.lua
+++ lua/user/lspsettings/sourcekit.lua
@@ -1,6 +1,16 @@
 -- vim.cmd set omnifunc=v:lua.vim.lsp.omnifunc once a buffer is opened
+
+local function cps()
+  local capabilities = vim.lsp.protocol.make_client_capabilities()
+  capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
+  capabilities.textDocument.completion.completionItem.snippetSupport = true
+  capabilities.workspace.didChangeWatchedFiles.dynamicRegistration = true
+  return capabilities
+end
+
 return {
   cmd = { '/usr/bin/sourcekit-lsp' },
+  capabilities = cps(),
   -- cmd = { '/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/sourcekit-lsp' },
   -- root_dir = function(filename, bufnr)
   --   -- Below fixes lsp root_dir when modules are nested within xcode projects
