23c9228cdf8f5dbf0421ad550a184d35a651c7a2
casss-dev <bcassidy50@gmail.com>
2024-07-21 14:59:11 -0500

    Fix deprecations

--- lua/user/maximize.lua
+++ lua/user/maximize.lua
@@ -4,9 +4,6 @@ local M = {
 
 function M.config()
   local m = require 'maximize'
-  m.setup {
-    default_keymaps = false,
-  }
   vim.keymap.set('n', '<leader>mw', m.toggle, { desc = 'toggle [M]aximize [W]indow' })
 end
 
