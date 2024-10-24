05a7d27e715efb93f11e56c1bf01ebcde988324c
casss-dev <bcassidy50@gmail.com>
2024-10-24 06:44:19 -0500

    Comment out super tab keymap

--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -125,8 +125,8 @@ local M = {
             cmp.select_next_item()
           elseif luasnip.expandable() then
             luasnip.expand()
-          elseif luasnip.expand_or_jumpable() then
-            luasnip.expand_or_jump()
+          -- elseif luasnip.expand_or_jumpable() then
+          --   luasnip.expand_or_jump()
           elseif check_backspace() then
             fallback()
           -- require("neotab").tabout()
@@ -141,8 +141,8 @@ local M = {
         ['<S-Tab>'] = cmp.mapping(function(fallback)
           if cmp.visible() then
             cmp.select_prev_item()
-          elseif luasnip.jumpable(-1) then
-            luasnip.jump(-1)
+          -- elseif luasnip.jumpable(-1) then
+          --   luasnip.jump(-1)
           else
             fallback()
           end
