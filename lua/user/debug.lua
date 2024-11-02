5c42bbf8f035ce3772248eb699e5a43ea5f1c728
casss-dev <bcassidy50@gmail.com>
2024-11-02 06:20:41 -0500

    Change breakpoint hotkeys

--- lua/user/debug.lua
+++ lua/user/debug.lua
@@ -46,16 +46,25 @@ function M.config()
   }
 
   -- Basic debugging keymaps, feel free to change to your liking!
+  vim.keymap.set('n', '<F5>', dap.continue, { desc = 'Debug: Start/Continue' })
+
   vim.keymap.set('n', '<F6>', function()
     dap.terminate()
     dapui.close()
   end, { desc = 'Debug: Terminate' })
-  vim.keymap.set('n', '<F5>', dap.continue, { desc = 'Debug: Start/Continue' })
+
   vim.keymap.set('n', '<F1>', dap.step_into, { desc = 'Debug: Step Into' })
   vim.keymap.set('n', '<F2>', dap.step_over, { desc = 'Debug: Step Over' })
   vim.keymap.set('n', '<F3>', dap.step_out, { desc = 'Debug: Step Out' })
-  vim.keymap.set('n', '<leader>b', dap.toggle_breakpoint, { desc = 'Debug: Toggle Breakpoint' })
-  vim.keymap.set('n', '<leader>B', function()
+
+  vim.keymap.set('n', '<leader>bb', dap.toggle_breakpoint, { desc = 'Debug: Toggle breakpoint' })
+  vim.keymap.set('n', '<leader>bx', dap.clear_breakpoints, { desc = 'Debug: Clear breakpoints' })
+
+  vim.keymap.set('n', '<leader>bl', function()
+    dap.list_breakpoints(true)
+  end, { desc = 'Debug: List breakpoints' })
+
+  vim.keymap.set('n', '<leader>bc', function()
     dap.set_breakpoint(vim.fn.input 'Breakpoint condition: ')
   end, { desc = 'Debug: Set Breakpoint' })
 
